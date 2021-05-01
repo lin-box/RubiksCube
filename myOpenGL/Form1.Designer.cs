ebd55b13c1abb9535540bc7fd74b144143050726
Mohse Shina <mshinaaaa@gmail.com>
2021-05-02 00:47:56 +0300

    Revert "add some text box about the rotation keyboard options"
    This reverts commit 9b8cdc445b32e2729ec6c7b73d2022625194f5d5.

--- myOpenGL/Form1.Designer.cs
+++ myOpenGL/Form1.Designer.cs
@@ -43,10 +43,6 @@ namespace myOpenGL
             this.btnSecondXUp = new System.Windows.Forms.Button();
             this.btnFirstXUp = new System.Windows.Forms.Button();
             this.timerRepaint = new System.Windows.Forms.Timer(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // panel1
@@ -185,55 +181,11 @@ namespace myOpenGL
             this.timerRepaint.Interval = 10;
             this.timerRepaint.Tick += new System.EventHandler(this.timerRepaint_Tick);
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(573, 363);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 13);
-            this.textBox1.TabIndex = 30;
-            this.textBox1.Text = "Rotate cube UP press \'W\'";
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(573, 420);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 13);
-            this.textBox2.TabIndex = 31;
-            this.textBox2.Text = "Rotate cube DOWN press \'S\'";
-            // 
-            // textBox3
-            // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Location = new System.Drawing.Point(573, 401);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 13);
-            this.textBox3.TabIndex = 32;
-            this.textBox3.Text = "Rotate cube LEFT press \'A\'";
-            // 
-            // textBox4
-            // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Location = new System.Drawing.Point(573, 382);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(154, 13);
-            this.textBox4.TabIndex = 33;
-            this.textBox4.Text = "Rotate cube RIGHT press \'D\'";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(750, 528);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnThirdXDown);
             this.Controls.Add(this.btnSecondXDown);
             this.Controls.Add(this.btnThirdYRight);
@@ -253,7 +205,6 @@ namespace myOpenGL
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -272,10 +223,6 @@ namespace myOpenGL
         private System.Windows.Forms.Button btnSecondXUp;
         private System.Windows.Forms.Button btnFirstXUp;
         private System.Windows.Forms.Timer timerRepaint;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
